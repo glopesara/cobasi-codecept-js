783fdd810eeadf3d1f0c60a5d1e833ce67fd7888
gabriel araujo <gabriel.lopes@cobasi.com.br>
2023-05-12 16:25:34 -0300

    refatoramento dos nomes das pages

--- steps.d.ts
+++ steps.d.ts
@@ -1,20 +1,20 @@
 /// <reference types='codeceptjs' />
 type steps_file = typeof import('./steps_file.js');
-type homeScreen = typeof import('./e2e/support/pages/home_screen.js');
+type comprarScreen = typeof import('./e2e/support/pages/home_screen.js');
 type menuMaisScreen = typeof import('./e2e/support/pages/menu_mais_screen.js');
 type loginScreen = typeof import('./e2e/support/pages/login_screen.js');
-type descontos_screen = typeof import('./e2e/support/pages/descontos_screen.js');
-type servicos_screen = typeof import('./e2e/support/pages/servicos_screen.js');
-type compra_programada_screen = typeof import('./e2e/support/pages/compra_programada_screen.js');
-type amigo_cobasi_screen = typeof import('./e2e/support/pages/amigo_cobasi_screen.js');
-type espaco_pet_screen = typeof import('./e2e/support/pages/espaco_pet_screen.js');
-type inicio_screen = typeof import('./e2e/support/pages/inicio_screen.js');
+type descontosScreen = typeof import('./e2e/support/pages/descontos_screen.js');
+type servicosScreen = typeof import('./e2e/support/pages/servicos_screen.js');
+type compraProgramadaScreen = typeof import('./e2e/support/pages/compra_programada_screen.js');
+type amigoCobasiScreen = typeof import('./e2e/support/pages/amigo_cobasi_screen.js');
+type espacoPetScreen = typeof import('./e2e/support/pages/espaco_pet_screen.js');
+type inicioScreen = typeof import('./e2e/support/pages/inicio_screen.js');
 type apis = typeof import('./e2e/support/helpers/apis.js');
 type scroll = typeof import('./e2e/support/helpers/scroll.js');
 type hooks = typeof import('./e2e/support/helpers/hooks.js');
 
 declare namespace CodeceptJS {
-  interface SupportObject { I: I, current: any, homeScreen: homeScreen, menuMaisScreen: menuMaisScreen, loginScreen: loginScreen, descontos_screen: descontos_screen, servicos_screen: servicos_screen, compra_programada_screen: compra_programada_screen, amigo_cobasi_screen: amigo_cobasi_screen, espaco_pet_screen: espaco_pet_screen, inicio_screen: inicio_screen, apis: apis, scroll: scroll, hooks: hooks }
+  interface SupportObject { I: I, current: any, comprarScreen: comprarScreen, menuMaisScreen: menuMaisScreen, loginScreen: loginScreen, descontosScreen: descontosScreen, servicosScreen: servicosScreen, compraProgramadaScreen: compraProgramadaScreen, amigoCobasiScreen: amigoCobasiScreen, espacoPetScreen: espacoPetScreen, inicioScreen: inicioScreen, apis: apis, scroll: scroll, hooks: hooks }
   interface Methods extends Appium, Mochawesome {}
   interface I extends ReturnType<steps_file>, WithTranslation<Mochawesome> {}
   namespace Translation {
